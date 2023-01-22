1c4183993af68961b9572acd99aaf5b6d9c00cd2
suman <ronaldosuman97@gmail.com>
2023-01-22 20:42:51 +0530

    add: navbar links, activeclass, arrow

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,11 +1,39 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "./NavBar.module.scss";
 
 export function NavBar() {
+  const route = useRouter().route;
+
   return (
     <nav className={styles.container}>
       <div className={styles.logo_container}>
         <img src="./logo.svg" />
       </div>
+
+      <ul>
+        <Link className={styles.link_con} href="/">
+          <li className={getLinkClassName("/", route)}>Recent Movies</li>
+        </Link>
+        <Link className={styles.link_con} href="/suggestions">
+          <li className={getLinkClassName("/suggestions", route)}>
+            Suggestions
+          </li>
+        </Link>
+        <Link className={styles.link_con} href="/favourites">
+          <li className={getLinkClassName("/favourites", route)}>Favourites</li>
+        </Link>
+        <Link className={styles.link_con} href="/watchlater">
+          <li className={getLinkClassName("/watchlater", route)}>
+            Watch Later
+          </li>
+        </Link>
+      </ul>
     </nav>
   );
+}
+
+function getLinkClassName(match: string, route: string) {
+  if (route.includes(match)) return `${styles.link} ${styles.active}`;
+  else return styles.link;
 }
