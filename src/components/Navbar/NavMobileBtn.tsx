5279b33f15fa5e8f347bd01f87b6f9c8bd4c35c2
suman <ronaldosuman97@gmail.com>
2023-01-25 21:03:50 +0530

    add: mbile navBtn not update

--- src/components/Navbar/NavMobileBtn.tsx
+++ src/components/Navbar/NavMobileBtn.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { FriendRequest } from "../Notification/FriendRequest";
+import { Suggestion } from "../Notification/Suggestion";
 import { ModalLink } from "./ModalLink";
 import styles from "./NavMobileBtn.module.scss";
 import { User } from "./User";
@@ -6,14 +8,21 @@ import { UserBtnBox } from "./UserBtnBox";
 
 export function NavMobileBtn() {
   const [userModal, setUserModal] = useState(false);
+  const [notModal, setNotModal] = useState(false);
 
   function handleUserClick() {
+    setNotModal(false);
     setUserModal((p) => !p);
+  }
+
+  function handleNotClick() {
+    setUserModal(false);
+    setNotModal((p) => !p);
   }
 
   return (
     <div className={styles.container}>
-      <button className={styles.not_btn}>
+      <button className={styles.not_btn} onClick={handleNotClick}>
         <img src="/navbar/bell.svg" className={styles.bell_icon} />
       </button>
       <button onClick={handleUserClick} className={styles.threedot_btn}>
@@ -21,8 +30,9 @@ export function NavMobileBtn() {
         <div id={userModal ? styles.active_line_2 : styles.line_2}></div>
         <div id={userModal ? styles.active_line_3 : styles.line_3}></div>
       </button>
+
       {userModal && (
-        <div className={styles.modal_user}>
+        <div className={styles.modal}>
           <div className={styles.top_container}>
             <User />
           </div>
@@ -34,6 +44,28 @@ export function NavMobileBtn() {
           </div>
           <div className={styles.bottom_container}>
             <UserBtnBox />
+          </div>
+        </div>
+      )}
+
+      {notModal && (
+        <div className={styles.modal}>
+          <div className={styles.notifications_box}>
+            <p>NOTIFICATIONS</p>
+            <div>{2}</div>
+          </div>
+          <div className={styles.not_scroll_box}>
+            <FriendRequest
+              image="https://api.dicebear.com/5.x/bottts-neutral/svg?seed=Scooter&scale=80"
+              name="John Milner"
+              time="January 23rd 2023, 3:16:59 pm"
+            />
+            <Suggestion
+              image="https://api.dicebear.com/5.x/bottts-neutral/svg?seed=suman&scale=80"
+              name="John Milner"
+              time="January 23rd 2023, 3:16:59 pm"
+              movie="Django Unchained"
+            />
           </div>
         </div>
       )}
