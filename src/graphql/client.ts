c81487008402828ca4d421690467c4118c25dbb0
suman <ronaldosuman97@gmail.com>
2023-01-29 19:38:04 +0530

    fix: stopped tracking .env File

---
+++ src/graphql/client.ts
@@ -0,0 +1,10 @@
+import {
+    ApolloClient,
+    InMemoryCache,
+} from "@apollo/client";
+
+export const client = new ApolloClient({
+    uri: "https://flyby-gateway.herokuapp.com/",
+    cache: new InMemoryCache(),
+});
+
