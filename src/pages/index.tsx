c73693db552b8911452e4d84b43e435ebac94062
suman <ronaldosuman97@gmail.com>
2023-01-31 11:34:57 +0530

    refactor: homepage unnecessary imports, functions deleted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,43 +3,12 @@ import { NavBar } from "@/components/Navbar/NavBar";
 import { Movie } from "@/components/Movie/Movie";
 import { MainContent } from "@/layouts/MainContent";
 import { MovieGrid } from "@/layouts/MovieGrid/MovieGrid";
-import { useLazyQuery, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { GET_MOVIES } from "@/graphql/queries/movieQueries";
-import { useContext, useEffect, useState } from "react";
-import { GET_AUTH_USER, GET_USER } from "@/graphql/queries/userQueries";
-import { userContext } from "./_app";
-import { app } from "@/firebase/firebaseConfig";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 import styles from "@/styles/Home.module.scss";
 
 export default function Home() {
-  const [firstRun, setFirstRun] = useState(true);
-  const [getAuthUser, userRes] = useLazyQuery(GET_AUTH_USER);
   const movieRes = useQuery(GET_MOVIES, { variables: { limit: 12 } });
-  const userctx = useContext(userContext);
-
-  useEffect(() => {
-    // handleGetAuthUser();
-  }, []);
-
-  function handleGetAuthUser() {
-    const auth = getAuth(app);
-
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const setUser = userctx?.setUser!;
-
-        if (firstRun) {
-          getAuthUser({
-            variables: { email: user.email },
-            onCompleted: (data) => setUser(data.get_authuser),
-          });
-          setFirstRun(false);
-        }
-      } else {
-      }
-    });
-  }
 
   if (movieRes.error) return <p>Error : {movieRes.error.message}</p>;
 
@@ -53,10 +22,7 @@ export default function Home() {
       </Head>
       <NavBar />
 
-      <MainContent
-        title="Recent Movies"
-        isLoading={movieRes.loading && userRes.loading}
-      >
+      <MainContent title="Recent Movies" isLoading={movieRes.loading}>
         <MovieGrid>
           {movieRes?.data?.get_movies.map((movie: any) => {
             return (
