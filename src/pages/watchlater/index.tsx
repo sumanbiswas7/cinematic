ccb57d12311dd8c558192d9cf05226521002d894
suman <ronaldosuman97@gmail.com>
2023-02-06 20:09:56 +0530

    add: watchlater page features

--- src/pages/watchlater/index.tsx
+++ src/pages/watchlater/index.tsx
@@ -1,14 +1,59 @@
+import { Movie } from "@/components/Movie/Movie";
 import { NavBar } from "@/components/Navbar/NavBar";
 import { MainContent } from "@/layouts/MainContent";
+import { MovieGrid } from "@/layouts/MovieGrid/MovieGrid";
+import { useState, useEffect } from "react";
 import styles from "./WatchLater.module.scss";
 
 export default function WatchLater() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    firstLoad();
+  }, []);
+
+  async function firstLoad() {
+    const moviesString = localStorage.getItem("watchlater");
+    const parsedMovies = JSON.parse(moviesString || "[]");
+    setMovies(parsedMovies);
+    console.log(parsedMovies);
+    setLoading(false);
+  }
+
   return (
     <>
       <NavBar />
-      <MainContent title="Watch Later">
-        <p>Watch Later</p>
+      <MainContent title="Favourites" isLoading={loading}>
+        <MovieGrid>
+          {movies.length ? (
+            movies.map((m: Movie) => {
+              return (
+                <Movie
+                  key={m.id}
+                  id={m.id}
+                  imageUrl={m.image}
+                  name={m.name}
+                  rating={m.rating}
+                  type={m.type}
+                />
+              );
+            })
+          ) : (
+            <p className={styles.nofav_text}>
+              You don&apos;t have any movies in List 😔
+            </p>
+          )}
+        </MovieGrid>
       </MainContent>
     </>
   );
+}
+
+interface Movie {
+  id: number;
+  rating: number;
+  name: string;
+  image: string;
+  type: string;
 }
