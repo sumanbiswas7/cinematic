41555af88ee5461abc64c663f80cd441b75b7ed1
suman <ronaldosuman97@gmail.com>
2023-02-13 13:50:41 +0530

    add: form skeleton updates

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -31,60 +31,64 @@ export function NewMovie() {
 
   return (
     <div className={styles.container}>
-      <TopNavBar />
-      <form className={styles.form_container}>
-        <div className={styles.two_row}>
-          <TextInput
-            label="Name"
-            withAsterisk
-            placeholder="Pulp Fiction"
-            classNames={{ input: styles.input }}
-          />
-          <TextInput
-            label="Director"
-            withAsterisk
-            placeholder="Quentin Tarantino"
-            classNames={{ input: styles.input }}
-          />
-        </div>
-        <div className={styles.two_row}>
-          <TextInput
-            label="Release Year"
-            withAsterisk
-            placeholder="1994"
-            classNames={{ input: styles.input }}
-          />
+      {/* <TopNavBar /> */}
+      <div className={styles.from_flex_container}>
+        <img className={styles.movie_img} src="/upload/upload.jpg" />
+        <form className={styles.form_container}>
+          <div className={styles.two_row}>
+            <TextInput
+              label="Name"
+              withAsterisk
+              placeholder="Pulp Fiction"
+              classNames={{ input: styles.input }}
+            />
+            <TextInput
+              label="Director"
+              withAsterisk
+              placeholder="Quentin Tarantino"
+              classNames={{ input: styles.input }}
+            />
+          </div>
+          <div className={styles.two_row}>
+            <TextInput
+              label="Release Year"
+              withAsterisk
+              placeholder="1994"
+              classNames={{ input: styles.input }}
+            />
+            <MultiSelect
+              data={movieGenres}
+              label="Genre"
+              withAsterisk
+              placeholder="Crime, Drama"
+              classNames={{ input: styles.input }}
+              searchable
+            />
+          </div>
           <MultiSelect
-            data={movieGenres}
-            label="Genre"
+            data={casts}
+            creatable
+            label="Casts"
             withAsterisk
-            placeholder="Crime, Drama"
+            placeholder="John Travolta, Samuel L. Jackson"
             classNames={{ input: styles.input }}
+            className={styles.margin_btm}
             searchable
+            getCreateLabel={(query) => `+ Add ${query}`}
+            onCreate={(cast) => {
+              setCasts([...casts, cast]);
+              return cast;
+            }}
           />
-        </div>
-        <MultiSelect
-          data={casts}
-          creatable
-          label="Casts"
-          withAsterisk
-          placeholder="John Travolta, Samuel L. Jackson"
-          classNames={{ input: styles.input }}
-          className={styles.margin_btm}
-          searchable
-          getCreateLabel={(query) => `+ Add ${query}`}
-          onCreate={(cast) => {
-            setCasts([...casts, cast]);
-            return cast;
-          }}
-        />
-        <Textarea
-          label="Description"
-          withAsterisk
-          placeholder="In the realm of underworld, a series of incidents intertwines the lives of two Los Angeles mobsters, a gangster's wife, a boxer and two small-time criminals."
-          classNames={{ input: styles.areainput }}
-        />
-      </form>
+          <Textarea
+            label="Description"
+            withAsterisk
+            placeholder="In the realm of underworld, a series of incidents intertwines the lives of two Los Angeles mobsters, a gangster's wife, a boxer and two small-time criminals."
+            classNames={{ input: styles.areainput }}
+          />
+          <button className={styles.upload_btn}>Upload</button>
+        </form>
+      </div>
     </div>
   );
 }
