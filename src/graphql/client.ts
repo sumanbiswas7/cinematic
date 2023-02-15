1fa46281cc04d9f5521c066e61e5295a8ed19a70
suman <ronaldosuman97@gmail.com>
2023-02-15 13:30:01 +0530

    fix: attempt 2 cors header changes

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -6,6 +6,6 @@ import {
 export const client = new ApolloClient({
     uri: process.env.NEXT_PUBLIC_SERVER_URL,
     cache: new InMemoryCache(),
-    headers: { apikey: process.env.NEXT_PUBLIC_API_KEY || "", 'Access-Control-Allow-Origin': "*" }
+    headers: { apikey: process.env.NEXT_PUBLIC_API_KEY || "" }
 });
 
