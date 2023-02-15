2c80db67bff968a2975c536359f72dc7a65509b7
suman <ronaldosuman97@gmail.com>
2023-02-15 12:45:47 +0530

    add: env apikey header added

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -6,6 +6,6 @@ import {
 export const client = new ApolloClient({
     uri: process.env.NEXT_PUBLIC_SERVER_URL,
     cache: new InMemoryCache(),
-
+    headers: { apikey: process.env.NEXT_PUBLIC_API_KEY || "" }
 });
 
